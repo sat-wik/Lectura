bc7c8b82373cd793aa9fe4dabd73be234bb7007b
satwik <satwik@berkeley.edu>
2024-06-20 16:25:31 -0700

    Untrack node_modules directory in backend

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to QuizCraft API!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
